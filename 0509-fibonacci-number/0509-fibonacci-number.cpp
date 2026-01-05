e0c3d34242bb57915948e7ebb56601b3adf90148
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-01-05 20:13:30 +0530

    Time: 13 ms (29.69%), Space: 7.8 MB (36.3%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int fib(int n) {
         if (n <= 1) return n;
-        return fib(n - 1) + fib(n - 2);
+        int last= fib(n-1);
+        int second_last= fib(n-2);
+        return last+ second_last;
     
 }
 };
