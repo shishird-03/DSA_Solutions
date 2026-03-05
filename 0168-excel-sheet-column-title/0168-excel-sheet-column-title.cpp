195440890d1945fcab8f325d48a93cccd636b3ec
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-03-05 23:53:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (24.1%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+      if (columnNumber ==0){
+        return "";
+      }
+      columnNumber--;
+        return convertToTitle(columnNumber/26)+char('A' + columnNumber % 26);
+      }
+        
+    
+};
