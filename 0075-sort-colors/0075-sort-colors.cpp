a866c2933fd7716908eb25aa46c8fc8eb59fb9c8
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-04-23 23:52:08 +0530

    Time: 2 ms (3.23%), Space: 11.6 MB (77.97%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count =0;
+        int n =nums.size();
+        for(int i=0;i<n;i++){
+            
+                sort(nums.begin(),nums.end());
+               
+            }
+        }
+    
+};
